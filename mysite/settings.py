a8c2940dbced794b653fc9c9631f7da46f65f14d
Devin <devinb5@uw.edu>
2016-07-29 02:34:36 +0000

    changed the database to mysql, protected password

--- mysite/settings.py
+++ mysite/settings.py
@@ -77,8 +77,12 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'devinb5Project',
+	'USER': 'devinb5',
+	'PASSWORD': 'Abcdefghi',
+	'HOST': 'localhost',
+	'PORT': '',
     }
 }
 
